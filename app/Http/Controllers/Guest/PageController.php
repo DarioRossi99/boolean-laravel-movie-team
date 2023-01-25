1f84e2e542b1f4ab17b09567c2e81a615e97730b
DarioRossi99 <rssdra99@gmail.com>
2023-01-25 16:54:49 +0100

    rimozione welcom

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+// use App\Models\train;
+
+class PageController extends Controller {
+    
+    function home() {
+
+        // $train = train::all();
+
+        // dump($train);
+
+        return view('home',[
+            // "train" => $train,
+        ]);
+    }
+
+}
